10074394509b8309c5d6f01dcf47db5b55c383d4
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-11-22 14:55:44 -0300

    generating new password on modal opening

---
+++ renderer/components/dashboard/NewPasswordModal.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { useEffect, useState } from "react"
+
+export default function NewPasswordModal({ setModalOpen }) {
+  const [password, setPassword] = useState('')
+
+  useEffect(() => {
+    generateNewPassword()
+  }, [])
+
+  function generateNewPassword() {
+    event.preventDefault();
+    const newPassword = Math.random().toString(36).slice(-10)
+    setPassword(newPassword)
+  }
+
+  return (
+    <div>
+      <h1>New Password</h1>
+      <form className="flex flex-col w-1/2">
+        <label htmlFor="name">Service:</label>
+        <input type="text" name="name" id="name" className="text-gray-900" />
+        <div className="flex">
+          <p>Password: {password}</p>
+          <button onClick={() => generateNewPassword()} className="ml-2">
+            <svg className="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 1v5h-5M2 19v-5h5m10-4a8 8 0 0 1-14.947 3.97M1 10a8 8 0 0 1 14.947-3.97"/></svg>
+          </button>
+        </div>
+        <button type="submit">Save</button>
+        <button type="button" onClick={() => setModalOpen(false)}>Cancel</button>
+      </form>
+    </div>
+  )
+}
--- renderer/pages/dashboard.tsx
+++ renderer/pages/dashboard.tsx
@@ -1,9 +1,22 @@
-export default function Dashboard() {
+import { useState } from "react"
+import NewPasswordModal from "../components/dashboard/NewPasswordModal"
 
+export default function Dashboard() {
+  const [modalOpen, setModalOpen] = useState(false)
+  const [password, setPassword] = useState('')
 
   return (
     <div>
       <h1>Welcome back, [user]</h1>
+      <button onClick={() => setModalOpen(true)}>Add New Password</button>
+      <p>{password}</p>
+      {
+        modalOpen && (
+          <div>
+            <NewPasswordModal setModalOpen={setModalOpen} />
+          </div>
+        )
+      }
     </div>
   )
 }
